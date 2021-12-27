df1f9b83bbcae8961d14d36a24682c077b17f507
meng-mao <meng.mao26gmail.com>
2021-12-27 19:19:12 +0700

    Fix code

--- project.py
+++ project.py
@@ -18,18 +18,19 @@ banner = tk.PhotoImage(file="Images\Game.png")
 canvas.create_image(0, 0, image=banner, anchor="nw")
 grid = [
     [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-    [4, 0, 1, 3, 0, 0, 0, 3, 2, 4],
-    [4, 3, 0, 0, 0, 3, 0, 0, 0, 4],
-    [4, 3, 0, 0, 0, 3, 0, 3, 0, 4],
-    [4, 0, 3, 3, 0, 3, 0, 0, 0, 4],
-    [4, 0, 2, 3, 2, 3, 0, 0, 3, 4],
-    [4, 0, 3, 0, 0, 3, 0, 3, 3, 4],
-    [4, 2, 0, 0, 3, 0, 0, 3, 0, 4],
-    [4, 3, 3, 3, 3, 0, 0, 0, 2, 4],
+    [4, 1, 3, 2, 0, 3, 0, 0, 0, 4],
+    [4, 0, 0, 3, 0, 0, 0, 3, 0, 4],
+    [4, 3, 0, 0, 3, 0, 0, 3, 0, 4],
+    [4, 2, 3, 0, 0, 0, 0, 3, 0, 4],
+    [4, 0, 3, 0, 3, 3, 0, 3, 0, 4],
+    [4, 0, 0, 0, 3, 0, 0, 3, 0, 4],
+    [4, 0, 3, 3, 0, 0, 3, 3, 0, 4],
+    [4, 2, 3, 2, 0, 3, 2, 0, 0, 4],
     [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
 ]
 isGameOver = False
 isGameWin=False
+# ==================Function draw grid=======================================
 def drawGrid():
         global isGameOver,isGameWin
         if isGameOver == False and isGameWin==False:
@@ -77,7 +78,7 @@ def close():
         LostGame()
     elif Score==5 or counter>=0:
         WinGame()
-counter = 60
+counter = 30
 button_label = tk.StringVar()
 button_label.set(counter)
 myTimer=tk.Button( textvariable=button_label, command=close).pack()
@@ -87,56 +88,64 @@ def deplay():
     drawGrid()
 def displayButton():
     # button start to play
-    my_button = tk.Button(root, text="Play", command=deplay)
-    my_button.config(width=7, height=1, bg="#007EE9",fg="yellow",border="5", font=("Arial", 20, "bold"))
-    my_button_canvas = canvas.create_window(630, 450, anchor="nw", window=my_button, tags="button")
+    myButton = tk.Button(root, text="Play", command=deplay)
+    myButton.config(width=7, height=1, bg="#007EE9",fg="yellow",border="5", font=("Arial", 20, "bold"))
+    myButton_canvas = canvas.create_window(630, 450, anchor="nw", window=myButton, tags="button")
 #=====================================button exit ---------------------
-    my_button = tk.Button(root, text="Exit", command=root.destroy)
-    my_button.config(width=7, height=1, bg="#007EE9",fg="yellow",border="5",font=("Arial", 20, "bold"))
-    my_button_canvas = canvas.create_window(630, 520, anchor="nw", window=my_button)
+    myButton = tk.Button(root, text="Exit", command=root.destroy)
+    myButton.config(width=7, height=1, bg="#007EE9",fg="yellow",border="5",font=("Arial", 20, "bold"))
+    myButton_canvas = canvas.create_window(630, 520, anchor="nw", window=myButton)
 displayButton()
-# ===================================find row ------------------------------------------------
+# ===================================find row of marion------------------
 def findRow(array):
     for row in range(len(array)):
         if 1 in array[row]:
             return row
+# =========================Find column of mario===========================
 def findCol(array):
     for row in range(len(array)):
         if 1 in array[row]:
             for col in range(len(array[row])):
                 if array[row][col] == 1:
                     return col
+# ========================Find row of diamond=============================
 def findRowDiamond(array):
     for row in range(len(array)):
         if 2 in array[row]:
             return row
+# =========================Find column of diamond===========================
 def findColDiamond(array):
     for row in range(len(array)):
         if 2 in array[row]:
             for col in range(len(array[row])):
                 if array[row][col] == 2:
                     return col
+# ======================Fine row of enemy============================
 def findRowOfEnemy(array):
     for row in range(len(array)):
         if 3 in array[row]:
             return row
+# ====================Find column of enemy===========================
 def findColOfEnemy(array):
     for row in range(len(array)):
         if 3 in array[row]:
             for col in range(len(array[row])):
                 if array[row][col] == 3:
                     return col
+# ==============Game Over==========================================
 def LostGame():
     global isGameOver
     isGameOver = True
     canvas.create_text(660,300,text="Game Over!!",font=("",60),fill="red")
     LostGame()
 Score = 0
+# ================Game Vactory======================================
 def WinGame():
     global isGameWin,Score,startMusic
     isGameWin = True
     canvas.create_text(660,300,text="You Win!!",font=("",60),fill="red")
     WinGame()
+# ========================Display Scores=================================
 def displayScore():
     global Score
     canvas.create_text(1200,200,text="Score",font=("",20),fill="black")
@@ -145,7 +154,7 @@ def displayScore():
         Score+=1
         canvas.itemconfig(myText,text=Score)
     displayScore()
-# ====================Move Right-========================================================
+# ====================Move Right-===========================================
 def moveRight(event):
     global isGameOver, isGameWin,Score,startMusic
     if isGameOver == False and isGameWin==False:
@@ -171,7 +180,6 @@ def moveRight(event):
                 grid[Row1][Col1] = 0
                 WinGame()             
         drawGrid()
-        print(Score)
 # =================================move left-----------------------
 def moveLeft(event):
     global Score,startMusic,isGameOver,isGameWin
@@ -199,8 +207,7 @@ def moveLeft(event):
                 grid[Row1][Col1] = 0
                 WinGame()
         drawGrid()
-        print(Score)
-# ===============================move up------------------------------
+# ===============================Move up------------------------------
 def moveUp(event):
     global Score,startMusic,isGameOver
     if isGameOver == False and isGameWin==False:
@@ -227,10 +234,9 @@ def moveUp(event):
                 grid[Row1][Col1] = 0
                 WinGame()
         drawGrid()
-        print(Score)
 # ==========================move Down----------------------------
 def moveDown(event):
-    global Score,startMusic,isGameOver
+    global Score,startMusic,isGameOver,Row1
     if isGameOver == False and isGameWin==False:
         Row1 = findRow(grid)
         Col1 = findCol(grid)
@@ -255,7 +261,6 @@ def moveDown(event):
                 grid[Row1][Col1] = 0
                 WinGame()
         drawGrid()
-        print(Score)
 # ==================Display=========================================
 root.bind("<Down>", moveDown)
 root.bind("<Up>", moveUp)
